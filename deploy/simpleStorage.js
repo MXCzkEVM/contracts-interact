7b9b42272eb1f38767e68e00b5827e7c25621cb3
Sheen <hx.sheen@gmail.com>
2023-05-28 00:11:56 +0200

    Update simpleStorage.js

--- deploy/simpleStorage.js
+++ deploy/simpleStorage.js
@@ -4,18 +4,10 @@ const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 require("dotenv").config()
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-    const { deploy } = deployments
+    const { deploy } = depds
     const { deployer } = await getNamedAccounts()
 
-    // const chainId = network.config.chainId
-
-    // gas乘以个 1.5 或者 2
-    // const gasPrice = (await ethers.provider.getGasPrice()).mul(5)
-    await deploy("SimpleStorage", {
-        from: deployer,
-        args: [],
-        log: true,
-        // gasPrice: 250000000000,
+    // const chainId = network.config.cha
     })
 }
 
